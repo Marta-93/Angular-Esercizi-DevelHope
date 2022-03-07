6ecff1329b622d8959119b62129a023b62e673b0
Marta Collu <marta.collu93@gmail.com>
2022-03-07 21:36:02 +0100

    fix:

--- README.md
+++
@@ -1,1 +0,0 @@
-# Angular-Esercizi-DevelHope
--- fdev/src/app/app.module.ts
+++ fdev/src/app/app.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { UserSingleComponent } from './user-single/user-single.component';
-import { UserListComponent } from './user-list/user-list.component';
+import { UserSingleComponent } from './user-container/user-list/user-single/user-single.component';
+import { UserListComponent } from './user-container/user-list/user-list.component';
 import { UserContainerComponent } from './user-container/user-container.component';
-import { UserDetailComponent } from './user-detail/user-detail.component';
+import { UserDetailComponent } from './user-container/user-detail/user-detail.component';
 import { OrderContainerComponent } from './order-container/order-container.component';
-import { OrderListComponent } from './order-list/order-list.component';
-import { OrderSingleComponent } from './order-single/order-single.component';
-import { OrderDetailComponent } from './order-detail/order-detail.component';
+import { OrderListComponent } from './order-container/order-list/order-list.component';
+import { OrderSingleComponent } from './order-container/order-single/order-single.component';
+import { OrderDetailComponent } from './order-container/order-detail/order-detail.component';
 
 @NgModule({
   declarations: [
